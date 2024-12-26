10327db0ea482eea8deda37ae88f9dce7fb08cdb
Choisehun3695 <choisehun3695@gmail.com>
2024-12-26 16:46:19 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+﻿#include "homework2.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<Animal*> animals;
+    animals.push_back(new Dog());
+    animals.push_back(new Cat());
+    animals.push_back(new Cow());
+
+    for (const auto& animal : animals) {
+        animal->makeSound();
+    }
+
+    for (auto& animal : animals) {
+        delete animal;
+    }
+
+    return 0;
+}
